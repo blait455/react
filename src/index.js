351a5119cd80eb8723594b2d41e606fbe9254c3f
blait455 <blait455@gmail.com>
2021-04-14 15:32:09 +0100

    DOM and bootstrap

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const element = <h1>hello world</h1>;
+ReactDom.render(element, document.getElementById('root'));
