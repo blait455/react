6924055a00fd62bcf4b07395f21ba421c9d38c3f
blait455 <blait455@gmail.com>
2021-04-14 15:42:22 +0100

    react components

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import Counter from './components/counter';
+
 const element = <h1>hello world</h1>;
-ReactDom.render(element, document.getElementById('root'));
+ReactDom.render(<Counter/>, document.getElementById('root'));
