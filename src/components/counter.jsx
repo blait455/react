27e34dce2d60dfb54853e49ad48d5afd70e1a804
blait455 <blait455@gmail.com>
2021-04-14 15:58:39 +0100

    expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 
 
 class Counter extends Component {
+    state = {
+        count: 2
+    };
     render(){
-        return(<h1>Counter component</h1>)
+        return(
+            <React.Fragment>
+                <h1>Counter component</h1>
+                <span>{ this.change() }</span><br/>
+                <button>Increment</button>    
+            </React.Fragment>
+        )
+    }
+    change(){
+        return this.state.count === 0 ? 'Zero' : this.state.count;
     }
 }
 
