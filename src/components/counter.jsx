bdfa4e7237b410e12c70cfe5656531ee18e4b167
blait455 <blait455@gmail.com>
2021-04-14 16:12:49 +0100

    style attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,12 +6,15 @@ class Counter extends Component {
         count: 0,
         imageUrl: 'https://picsum.photos/200'
     };
+    styles = {
+        fontSize: 30,
+    }
     render(){
         return(
             <React.Fragment>
                 <img src={ this.state.imageUrl} alt=""/>
-                <span className="badge badge-primary m-2">{ this.change() }</span>
-                <button>Increment</button>    
+                <span style={this.styles} className="badge badge-primary m-2">{ this.change() }</span>
+                <button className="btn btn-primary">Increment</button>    
             </React.Fragment>
         )
     }
